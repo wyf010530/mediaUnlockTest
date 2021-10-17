b87ea2b01392048575a9615efa8a1eafbe624881
小飞 <61732538+wyf010530@users.noreply.github.com>
2021-10-17 10:45:53 +0800

    Create mediaUnlockTest.sh

---
+++ mediaUnlockTest.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+UA_Browser="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36";
+UA_Dalvik="Dalvik/2.1.0 (Linux; U; Android 9; ALP-AL00 Build/HUAWEIALP-AL00)";
+DisneyAuth="grant_type=urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Atoken-exchange&latitude=0&longitude=0&platform=browser&subject_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiNDAzMjU0NS0yYmE2LTRiZGMtOGFlOS04ZWI3YTY2NzBjMTIiLCJhdWQiOiJ1cm46YmFtdGVjaDpzZXJ2aWNlOnRva2VuIiwibmJmIjoxNjIyNjM3OTE2LCJpc3MiOiJ1cm46YmFtdGVjaDpzZXJ2aWNlOmRldmljZSIsImV4cCI6MjQ4NjYzNzkxNiwiaWF0IjoxNjIyNjM3OTE2LCJqdGkiOiI0ZDUzMTIxMS0zMDJmLTQyNDctOWQ0ZC1lNDQ3MTFmMzNlZjkifQ.g-QUcXNzMJ8DwC9JqZbbkYUSKkB1p4JGW77OON5IwNUcTGTNRLyVIiR8mO6HFyShovsR38HRQGVa51b15iAmXg&subject_token_type=urn%3Abamtech%3Aparams%3Aoauth%3Atoken-type%3Adevice"
+DisneyHeader="authorization: Bearer ZGlzbmV5JmJyb3dzZXImMS4wLjA.Cu56AgSfBTDag5NiRA81oLHkDZfu5L3CKadnefEAY84"
+
+export LANG="en_US";
+export LANGUAGE="en_US";
+export LC_ALL="en_US";
+
+function InstallJQ() {
+    #安装JQ
+    if [ -e "/etc/redhat-release" ];then
+        echo -e "${Font_Green}正在安装依赖: epel-release${Font_Suffix}";
+        yum install epel-release -y -q > /dev/null;
+        echo -e "${Font_Green}正在安装依赖: jq${Font_Suffix}";
+        yum install jq -y -q > /dev/null;
+        elif [[ $(cat /etc/os-release | grep '^ID=') =~ ubuntu ]] || [[ $(cat /etc/os-release | grep '^ID=') =~ debian ]];then
+        echo -e "${Font_Green}正在更新软件包列表...${Font_Suffix}";
+        apt-get update -y > /dev/null;
+        echo -e "${Font_Green}正在安装依赖: jq${Font_Suffix}";
+        apt-get install jq -y > /dev/null;
+        elif [[ $(cat /etc/issue | grep '^ID=') =~ alpine ]];then
+        apk update > /dev/null;
+        echo -e "${Font_Green}正在安装依赖: jq${Font_Suffix}";
+        apk add jq > /dev/null;
+    else
+        echo -e "${Font_Red}请手动安装jq${Font_Suffix}";
+        exit;
+    fi
+}
+
+function PharseJSON() {
+    # 使用方法: PharseJSON "要解析的原JSON文本" "要解析的键值"
+    # Example: PharseJSON ""Value":"123456"" "Value" [返回结果: 123456]
+    echo -n $1 | jq -r .$2;
+}
+
+function GameTest_Steam(){
+    echo -n -e " Steam Currency:\t\t\t->\c";
+    local result=`curl --user-agent "${UA_Browser}" -${1} -fsSL --max-time 30 https://store.steampowered.com/app/761830 2>&1 | grep priceCurrency | cut -d '"' -f4`;
+    
+    if [ ! -n "$result" ]; then
+        echo -n -e "\r Steam Currency:\t\t\t${Font_Red}Failed (Network Connection)${Font_Suffix}\n" && echo -e " Steam Currency:\t\t\tFailed (Network Connection)" >> ${LOG_FILE};
+    else
+        echo -n -e "\r Steam Currency:\t\t\t${Font_Green}${result}${Font_Suffix}\n" && echo -e " Steam Currency:\t\t\t${result}" >> ${LOG_FILE};
+    fi
+}
